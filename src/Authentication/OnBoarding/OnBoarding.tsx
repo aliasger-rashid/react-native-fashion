74f4c58e17969cb7f82890fa6aac5b04eb23882f
Ali Asger <aliasger619>
2021-04-28 01:27:51 +0300

    Add react navigation , Load Asset and onboarding

---
+++ src/Authentication/OnBoarding/OnBoarding.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+const OnBoarding = () => {
+  return <View style={{ flex: 1, backgroundColor: "cyan" }} />;
+};
+
+export default OnBoarding;
